16685bcba450d9496952eb59c82a8370208dde50
jayfoxfoxy <joao.p.s.pinho@gmail.com>
2020-02-19 23:40:32 +0000

    Added P5

---
+++ P5.py
@@ -0,0 +1,14 @@
+num = 1
+check = 0
+
+while check == 0:
+	if (num % 1 == 0 and num % 2 == 0 and num % 3 == 0 and num % 4 == 0
+	and num % 5 == 0 and num % 6 == 0 and num % 7 == 0 and num % 8 == 0
+	and num % 9 == 0 and num % 10 == 0 and num % 11 == 0 
+	and num % 12 == 0 and num % 13 == 0 and num % 14 == 0 
+	and num % 15 == 0 and num % 16 == 0 and num % 17 == 0
+	and num % 18 == 0 and num % 19 == 0 and num % 20 == 0):
+		check = 1	
+	num += 1
+
+print(num - 1)
--- README.md
+++ README.md
@@ -5,4 +5,5 @@ P1:233168
 P2:4613732  
 P3: 6857  
 	Prime Factor of a number is the sum of numbers that result 0 on mod and not bigger than the number themself;  
-P4: 906609
+P4: 906609  
+P5: 232792560
